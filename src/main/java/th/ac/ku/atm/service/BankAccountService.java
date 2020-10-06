b32db922f6ac46338f6cfb5bdf636eb6205ee38f
plumest <chayathon.k@ku.th>
2020-10-06 21:16:11 +0700

    AJAX Bankaccount from the API

--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -1,27 +1,45 @@
 package th.ac.ku.atm.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import th.ac.ku.atm.model.BankAccount;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class BankAccountService {
 
-    private List<BankAccount> bankAccountsList;
+    private RestTemplate restTemplate;
 
-    @PostConstruct
-    public void postConstruct() {
-        this.bankAccountsList = new ArrayList<>();
+    public BankAccountService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public List<BankAccount> getCustomerBankAccount(int customerId) {
+        String url = "http://localhost:8091/api/bankaccount/customer/" +
+                customerId;
+        ResponseEntity<BankAccount[]> response =
+                restTemplate.getForEntity(url, BankAccount[].class);
+
+        BankAccount[] accounts = response.getBody();
+
+        return Arrays.asList(accounts);
     }
 
     public List<BankAccount> getBankAccounts() {
-        return new ArrayList<>(this.bankAccountsList);
+        String url = "http://localhost:8091/api/bankaccount/";
+        ResponseEntity<BankAccount[]> response = restTemplate.getForEntity(url, BankAccount[].class);
+
+        BankAccount[] accounts = response.getBody();
+
+        return Arrays.asList(accounts);
     }
 
     public void createBankAccount(BankAccount bankAccount) {
-        bankAccountsList.add(bankAccount);
+        // Nothing for now
     }
 }
